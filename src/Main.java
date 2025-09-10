04f866936f744f846b9106206b0d764f61d5099e
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-10 14:46:35 +0200

    Añadiendo proyecto AD

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.File;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(eDirectorio("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
+        System.out.println(eFicheiro("../../Descargas"));
+        System.out.println(eDirectorio("Hola mundo"));
+        System.out.println(eDirectorio("../../Descargas")); // Empieza en la carpeta del proyecto así que esto funciona
+        System.out.println(eFicheiro("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
+        System.out.println(eDirectorio("Hola mundo"));
+    }
+    public static String eDirectorio(String cadea) {
+        File archivo = new File(cadea);
+        if (archivo.isDirectory()) {
+            return "é directorio";
+        } else {
+            return "non é directorio";
+        }
+    }
+    public static String eFicheiro(String cadea) {
+        File archivo = new File(cadea);
+        if (archivo.isFile()) {
+            return "é directorio(ficheiro)";
+        } else {
+            return "non é directorio (ficheiro)";
+        }
+    }
+    public static void crearDirectorio() {}
+}
