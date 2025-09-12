6022c6b8789fc6b8efa771d30001967c3d9544dd
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-12 12:44:18 +0200

    Añadiendo los metodos del cuarto y quinto ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.io.File;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(eDirectorio("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
-        System.out.println(eFicheiro("../../Descargas"));
-        System.out.println(eDirectorio("Hola mundo"));
-        System.out.println(eDirectorio("../../Descargas")); // Empieza en la carpeta del proyecto así que esto funciona
-        System.out.println(eFicheiro("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
-        System.out.println(eDirectorio("Hola mundo"));
-        crearDirectorio("especimen");
+        //System.out.println(eDirectorio("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
+        //System.out.println(eFicheiro("../../Descargas"));
+        //System.out.println(eDirectorio("Hola mundo"));
+        //System.out.println(eDirectorio("../../Descargas")); // Empieza en la carpeta del proyecto así que esto funciona
+        //System.out.println(eFicheiro("../../Descargas/PSP_Tarea01.pdf")); // Empieza en la carpeta del proyecto así que esto funciona
+        //System.out.println(eDirectorio("Hola mundo"));
+        //crearDirectorio("especimen");
+        creaFicheiro("/home/dam/","Crar");
+        modoAcceso("/home/dam/","Crar");
+
     }
     public static String eDirectorio(String cadea) {
         File archivo = new File(cadea);
@@ -34,6 +38,33 @@ public class Main {
             System.out.println("Directorio creado");
         } else {
             System.out.println("Directorio no creado");
+        }
+    }
+    public static void creaFicheiro(String dirName,String fileName) {
+        File archivoCrear = new File(dirName+fileName);
+        try {
+            archivoCrear.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public static void modoAcceso(String dirName, String fileName) {
+        File archivo = new File(dirName+fileName);
+        if (archivo.canRead() && archivo.canWrite()) {
+            System.out.println("lectura si");
+            System.out.println("escritura si");
+        } else if (!archivo.canRead() && !archivo.canWrite()) {
+            System.out.println("lectura no");
+            System.out.println("escritura no");
+        }
+        else if (archivo.canRead()) {
+            System.out.println("lectura si");
+        } else if (archivo.canWrite()) {
+            System.out.println("Escritura si");
+        } else if (!archivo.canRead()) {
+            System.out.println("Lectura no");
+        } else if (!archivo.canWrite()) {
+            System.out.println("Escritura no");
         }
     }
 }
